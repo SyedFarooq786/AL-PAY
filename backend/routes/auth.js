93f3b96d591a65e0cde3b4b715290ea73997bf81
eatfit-sh <syed.farooq@eatfit.in>
2024-07-10 13:14:36 +0530

    new code

---
+++ backend/routes/auth.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0c7426307618335bee99d37997349f1c4a807e86e29e3f5648243e8b2acd0020
+size 1438
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3d281ff60fbb6ffb54a57a313cfa08e49b3d3a13ff1574d6f9ea35d8902ed7a
-size 440
+oid sha256:b81024ab43ef7ff6563fc51fe32f7dfa86057ba07057814e58c68ec594a1604e
+size 836
